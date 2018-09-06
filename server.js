c90a0ce070f244a6a069785513e5f92c7677934e
Matt Thomas <mmattthomas@gmail.com>
2018-09-06 11:22:27 -0700

    adding connect-history-api-fallback for router issue w heroku

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-// server.js
+var history = require('connect-history-api-fallback');
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
+app.use(history());    // needed for router to work in spa
 app.use(serveStatic(__dirname + ""));
 var port = process.env.PORT || 5000;
 app.listen(port);
