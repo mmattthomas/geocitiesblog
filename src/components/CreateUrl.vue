52564ed4bf79fe805555e54f5ba4f51d9a9f5f38
Matt Thomas <mmattthomas@gmail.com>
2018-09-08 16:05:35 -0700

    added firebase hosting config and url enforcement

--- src/components/CreateUrl.vue
+++ src/components/CreateUrl.vue
@@ -83,6 +83,13 @@ export default {
 
 			const dbg = false
 
+			// validate URL
+			if (!validateUrl(this.formData.url)) {
+				alert('you have entered an invalid URL - YOU MAY NOT ENTER')
+				return;
+			}
+
+
 			const timestamp = firebase.database.ServerValue.TIMESTAMP
 			let hoodSearchResult = null
 			firebase.database().ref('hoods').orderByChild('name').equalTo(this.formData.neighborhood).once('value',(snapshot)=>{
@@ -142,6 +149,10 @@ export default {
 	created(){			
 		this.fetchFirebaseData();					
 	}
+}
+
+function validateUrl(value) {
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
 }
 
 function snapshotToArrayHoods(snapshot) {
